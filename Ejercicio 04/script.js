784a8db0107f7491eb3682edac8c102919a01614
Javi Calonge <javicalongem@gmail.com>
2024-07-13 10:49:37 +0200

    Ejercicio04

---
+++ Ejercicio 04/script.js
@@ -0,0 +1,5 @@
+console.log("%cMétodos de arrays", "font-weight: bold", "color: red")
+
+// * provar mètodes concat(), join(), pop(), push(), shift(), unshift(), flat()
+
+// Para abrir emogis tecla "Windows" + "."
